b54b1461c03cc53e1a64c4e78dab9589a7d1b498
ermin <ermingarcia1077@gmail.com>
2026-08-25 00:54:02 +0800

    Create supabase-client.js

---
+++ assets/supabase-client.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://mgofvanhrfjzxhpwsoys.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_LD94Ab23HZtRGyNbf7sjmg_2N5sJSNi";
+
+const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
