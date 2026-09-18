38226c22a3c24f1b2ec273af2dca1c0f1c9d5d4f
ermin <ermingarcia1077@gmail.com>
2026-09-18 14:17:40 +0800

    Update events.js

--- events.js
+++ events.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 var eventsData = [
 { title: "National Students Convention", images: ["assets/nsc1.jpg", "assets/nsc2.jpg", "assets/nsc3.jpg", "assets/nsc4.jpg", "assets/nsc5.jpg"] },
-{ title: "Junior Students Convention", images: ["assets/convention1.png", "assets/jsc2.jpg", "assets/jsc3.jpg", "assets/jsc4.jpg", "jsc5.png", "jsc6.png"] },
+{ title: "Junior Students Convention", images: ["assets/convention1.png", "assets/jsc2.jpg", "assets/jsc3.jpg", "assets/jsc4.jpg", "jsc5.png"] },
 { title: "Nutrition Month", images: ["assets/nutrition1.png", "assets/nutrition2.png"] },
 { title: "Graduation & Recognition", images: ["assets/gr1.png", "assets/gr2.png"] },
 { title: "", images: ["assets/event5-1.png", "assets/event5-2.png"] },
